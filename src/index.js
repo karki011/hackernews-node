1abc127bbf956a7eaa183407f166463fd56e48ba
karki011 <karki011@gmail.com>
2019-01-13 10:26:30 -0600

    cleaned up code and added mutation for post

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info: String!
-}
-`
+
+
+let links= [{
+    id: 'link-0',
+    url: 'www.google.com',
+    description: 'Google home page'
+}]
+
+let idCount = links.length;
 
 const resolvers = { 
     Query: {
-        info: () => `This is the API of Hackernews cloner`
+        info: () => `This is the API of Hackernews cloner`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent,args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     }
+  
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
   })
   server.start(() => console.log(`Server is running on http://localhost:4000`))
